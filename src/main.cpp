c93435143a1413c50c96fec78a39a5c4d4be02e9
Mykola <nicvan@ukr.net>
2025-04-17 14:13:40 +0300

    setup lsp for vim

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,23 @@
-#include <M5GFX.h>
+#include "lgfx/v1/misc/enum.hpp"
 #include <epdiy.h>
+#include <M5GFX.h>
 
 static M5GFX display;
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("Starting Hello World on PaperS3...");
+	Serial.begin(115200);
 
-  display.init();
-  display.clearDisplay();
+	display.init();
+	display.clearDisplay();
 
-  // Configure text settings
-  display.setTextSize(2);
-  display.setTextColor(TFT_BLACK, TFT_WHITE);
-  display.setCursor(10, 10);
+	display.setTextSize(2);
+	display.setTextColor(TFT_BLACK, TFT_WHITE);
+	display.setCursor(10, 10);
+	
+	display.println("SUKA NAKINEC");
 
-  display.println("Hello, World!");
-
-  // For e-paper displays, update the screen to show the changes
-  display.display();
-  display.waitDisplay();
+	display.display();
+	display.waitDisplay();
 }
 
 void loop() {
